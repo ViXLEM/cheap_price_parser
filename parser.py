7c931f14e1373d27fe16cf800971f94860528e44
ViXLEM <victor.shutenko@gmail.com>
2018-02-21 21:25:16 +0200

    refactor codestyle

--- parser.py
+++ parser.py
@@ -21,14 +21,15 @@ class Supermarket():
         self.generate_url()
 
     def save_to_db(self):
-        start_time = time.time()
+        """This func save dict goods to personal db."""
+
         session = Session()
 
         for x in session.query(self.supermarket_orm).all():  # deactivate all products
             x.active = False
         session.commit()
 
-        for barcode in self.goods_dict.keys():
+        for barcode in self.goods_dict:
             product = session.query(self.supermarket_orm).filter_by(barcode=barcode).first()
 
             if product:
@@ -43,16 +44,17 @@ class Supermarket():
             session.add(product)
         session.commit()
         session.close()
-        end_time = time.time()
-        self.metadata['save_to_db_time'] = round(end_time-start_time, 2)
-        self.metadata['new_products_count'] = len(self.new_goods_barcode)
 
     def merge_with_main_db(self):
-        start_time = time.time()
+        """This func merge personal db with main goods db."""
+
         session = Session()
         for x in self.new_goods_barcode:
-            new_product = session.query(self.supermarket_orm).filter_by(barcode=x).first()
-            exist_product = session.query(Product).filter(Product.barcode == new_product.barcode).scalar()
+            new_product = session.query(self.supermarket_orm)\
+                                            .filter_by(barcode=x).first()
+            exist_product = session.query(Product)\
+                                   .filter(Product.barcode == new_product.barcode)\
+                                   .scalar()
             if exist_product:
                 exec('exist_product.barcode_{} = new_product.barcode'.format(self.supermarket_name))
             else:
@@ -62,27 +64,40 @@ class Supermarket():
                 exec('new_product.barcode_{} = new_product.barcode'.format(self.supermarket_name))
                 session.add(new_product)
             session.commit()
-        end_time = time.time()
-        self.metadata['merge_time'] = round(end_time-start_time, 2)
 
     def update(self):
+        """This func download and parse goods data
+        then save him to personal supermarket db
+        and merge personal db with main db."""
+
         start_time = time.time()
         self.goods_dict = self.get_goods_dict()
         end_time = time.time()
         self.metadata['download_time'] = round(end_time-start_time, 2)
         self.metadata['products_count'] = len(self.goods_dict)
+
+        start_time = time.time()
         self.save_to_db()
+        end_time = time.time()
+        self.metadata['save_to_db_time'] = round(end_time-start_time, 2)
+        self.metadata['new_products_count'] = len(self.new_goods_barcode)
+
+        start_time = time.time()
         self.merge_with_main_db()
+        end_time = time.time()
+        self.metadata['merge_time'] = round(end_time-start_time, 2)
 
     def get_metadata(self):
+        """Return dict with time and goods count values."""
         print(self.metadata)
+        return self.metadata
 
 
 class Auchan(Supermarket):
     def __init__(self):
-        self.auchan_categories = {'beer': 'beer', 'vodka': 'vodka', 'wine': 'wine',
-                                  'champagne': 'champagne-sparkling-wine',
-                                  'cognac': 'cognac'}
+        self.auchan_categories = {'beer': 'beer', 'vodka': 'vodka',
+                                  'wine': 'wine', 'cognac': 'cognac',
+                                  'champagne': 'champagne-sparkling-wine'}
         self.auchan_dict = {}
         super().__init__(AuchanProduct, 'auchan')
 
@@ -96,7 +111,8 @@ class Auchan(Supermarket):
             price = product['price'] / 100
             barcode = product['ean'][1:]
             # url = 'https://auchan.zakaz.ua/uk/0{}/'.format(barcode)
-            data_dict.update({barcode: {'name': name, 'price': price, 'category': category}})
+            data_dict.update({barcode: {'name': name, 'price': price,
+                                        'category': category}})
         return data_dict
 
     def get_goods_dict(self):
@@ -113,6 +129,8 @@ class Auchan(Supermarket):
         return data
 
     def generate_url(self):
+        """This func generate dynamic dict of urls for goods categories."""
+
         api_url = 'https://auchan.zakaz.ua/api/query.json'
         json_template = JSON_template.copy()
         for category_name in self.auchan_categories:
@@ -150,7 +168,8 @@ class MegaMarket(Supermarket):
             name = product_code_and_name.text
             price = product.find('div', class_='price').text.replace(
                 ' ', '')[1:]  # del white spaces and '/n'
-            data_dict.update({product_code: {'name': name, 'price': price, 'category': category}})
+            data_dict.update({product_code: {'name': name, 'price': price,
+                                             'category': category}})
         return data_dict
 
     def get_goods_dict(self):
@@ -168,6 +187,8 @@ class MegaMarket(Supermarket):
         return data
 
     def generate_url(self):
+        """This func generate dynamic dict of urls for goods categories."""
+
         for x in self.mm_categories:
             url_template = 'https://megamarket.ua/ua/catalogue/category/{0}?show=48000'
             url = url_template.format(self.mm_categories[x])
@@ -176,9 +197,9 @@ class MegaMarket(Supermarket):
 
 class Novus(Supermarket):
     def __init__(self):
-        self.novus_categories = {'beer': 'beer', 'vodka': 'vodka', 'wine': 'wine',
-                                 'champagne': 'champagne-sparkling-wine',
-                                 'cognac': 'cognac'}
+        self.novus_categories = {'beer': 'beer', 'vodka': 'vodka',
+                                 'wine': 'wine', 'cognac': 'cognac',
+                                 'champagne': 'champagne-sparkling-wine'}
         self.novus_dict = {}
         super().__init__(NovusProduct, 'novus')
 
@@ -199,8 +220,8 @@ class Novus(Supermarket):
             price_grn = prc.find('span', class_='grivna').text
             price_coin = prc.find('span', class_='kopeiki').text
             full_price = '{}.{}'.format(price_grn, price_coin)
-            data_dict.update(
-                {barcode: {'name': name, 'price': full_price, 'category': category}})
+            data_dict.update({barcode: {'name': name, 'price': full_price,
+                                        'category': category}})
         return data_dict
 
     def get_goods_dict(self):
@@ -216,6 +237,8 @@ class Novus(Supermarket):
         return data
 
     def generate_url(self):
+        """This func generate dynamic dict of urls for goods categories."""
+
         url_template = 'https://novus.zakaz.ua/uk/{category}/?&page={page_num}'
         for x in self.novus_categories:
             url_first_page = url_template.format(category=self.novus_categories[x],
